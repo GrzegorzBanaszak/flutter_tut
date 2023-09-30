0d02197e7f60d49b11149db9754dfde56c597be8
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2023-09-30 21:16:55 +0200

    First simple layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home:Text("Hello"),
-
+  runApp(MaterialApp(
+    home:Scaffold(
+      appBar: AppBar(
+        title: Text("Hello"),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text("Hello"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        child: Text("+"),
+      ),
+    )
   ));
 }
