49fea304634afb5ea7b5982b310d9962bcaa3a32
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2023-10-02 19:25:56 +0200

    Button and Icon example

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,10 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home:Home(),
+    home: Home(),
     debugShowCheckedModeBanner: false,
   ));
 }
-
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -15,31 +14,44 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Hello"),
+        title: const Text("Hello"),
         centerTitle: true,
         backgroundColor: Colors.red[300],
       ),
-      body: ListView(
-        children: <Widget>[
-          Image(
-            image: AssetImage("assets/cat-1.jpg"),
-          ),
-          Image(
-            image: AssetImage("assets/cat-2.jpg"),
-          ),
-          Image(
-            image: AssetImage("assets/cat-3.jpg"),
-          ),
-          Image.network(
-            "https://images.unsplash.com/photo-1543852786-1cf6624b9987?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
-
-          ),
-        ],
+      body: Center(
+        child: ElevatedButton.icon(
+          onPressed: () {
+            print("You click me");
+          },
+          style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(Colors.red)),
+          icon: const Icon(Icons.home),
+          label: const Text("Click me"),
+        ),
       ),
+
+      // Lista obrazów
+      // body: ListView(
+      //   children: <Widget>[
+      //     Image(
+      //       image: AssetImage("assets/cat-1.jpg"),
+      //     ),
+      //     Image(
+      //       image: AssetImage("assets/cat-2.jpg"),
+      //     ),
+      //     Image(
+      //       image: AssetImage("assets/cat-3.jpg"),
+      //     ),
+      //     Image.network(
+      //       "https://images.unsplash.com/photo-1543852786-1cf6624b9987?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
+      //
+      //     ),
+      //   ],
+      // ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
-        child: Text("+"),
+        onPressed: () {},
         backgroundColor: Colors.red[600],
+        child: const Text("+"),
       ),
     );
   }
