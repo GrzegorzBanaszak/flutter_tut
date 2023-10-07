3921bc66586d01dd990c19f6961c15cc75dcf40e
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2023-10-07 19:20:24 +0200

    List display example

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,38 @@
 import 'package:flutter/material.dart';
-import "./firstApp.dart";
 
 void main() {
   runApp(const MaterialApp(
-    home: Home(),
+    home: QuoteList(),
     debugShowCheckedModeBanner: false,
   ));
+}
+
+class QuoteList extends StatefulWidget {
+  const QuoteList({super.key});
+
+  @override
+  State<QuoteList> createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  List<String> quotes = [
+    'Be yourself; everyone else is already taken',
+    'I have nothing to declare except my genius',
+    'The truth is rarely pure and never simple'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: const Text("Awesome Quotes"),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((e) => Text(e)).toList(),
+      ),
+    );
+  }
 }
