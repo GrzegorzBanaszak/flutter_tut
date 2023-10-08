fe80d269a164238e1cf2b0c80d7e9732aa358066
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2023-10-08 20:02:00 +0200

    Starting the world time app

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_tut/quote_card.dart';
-import "./quote.dart";
+import "package:flutter_tut/pages/home.dart";
 
 void main() {
   runApp(const MaterialApp(
-    home: QuoteList(),
+    home: Home(),
     debugShowCheckedModeBanner: false,
   ));
-}
-
-class QuoteList extends StatefulWidget {
-  const QuoteList({super.key});
-
-  @override
-  State<QuoteList> createState() => _QuoteListState();
-}
-
-class _QuoteListState extends State<QuoteList> {
-  List<Quote> quotes = [
-    Quote(
-        author: 'Oscar Wilde',
-        text: 'Be yourself; everyone else is already taken'),
-    Quote(
-        author: 'Oscar Wilde',
-        text: 'I have nothing to declare except my genius'),
-    Quote(
-        author: 'Oscar Wilde',
-        text: 'The truth is rarely pure and never simple')
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: const Text("Awesome Quotes"),
-        centerTitle: true,
-        backgroundColor: Colors.redAccent,
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: quotes
-            .map((quoute) => QuoteCard(
-                quote: quoute,
-                delete: () {
-                  setState(() {
-                    quotes.remove(quoute);
-                  });
-                }))
-            .toList(),
-      ),
-    );
-  }
 }
